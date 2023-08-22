264a1a7f7941bf2317004d181e923eaac87fe94d
Marco Donna <marco.donna03@gmail.com>
2023-08-22 19:02:11 +0200

    created missing implementation error

---
+++ test/utils/test_missing_implementation_error.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+
+require_relative '../../src/utils/missing_implementation_error'
+
+class TestMissingImplementation < Test::Unit::TestCase
+    def test_raise
+        assert_raise(MissingImplementationError) { raise MissingImplementationError.new }
+        assert_raise(MissingImplementationError) { raise MissingImplementationError.new("Super useful message") }
+    end
+end
