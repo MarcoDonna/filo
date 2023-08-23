c58f9358df4984c7f3e0a29a718dbe210e8e8951
Marco Donna <marco.donna03@gmail.com>
2023-08-23 18:15:31 +0200

    created activation function super class

---
+++ src/activation_function/activation_function.rb
@@ -0,0 +1,34 @@
+require 'matrix'
+
+require_relative '../utils/missing_implementation_error'
+
+#Parent class to all activation functions.
+#Each activation function inherits this class and implements the function and derivative methods
+class ActivationFunction
+
+    def initialize config={}
+        @config = config
+    end
+
+    #Apply activation function to each row of the matrix.
+    #Applied to each row insted of single item is needed is some activation functions (softmax).
+    def apply_activation_function matrix
+        Matrix[*matrix.clone().to_a.map { |row| function(row) }]
+    end
+
+    #Apply activation function derivative to each row of the matrix.
+    #Applied to each row insted of single item is needed is some activation functions (softmax).
+    def apply_activation_function_derivative matrix
+        Matrix[*matrix.clone().to_a.map { |row| derivative(row) }]
+    end
+
+    private
+
+    def function vector
+        raise MissingImplementationError.new
+    end
+
+    def derivative vector
+        raise MissingImplementationError.new
+    end
+end
