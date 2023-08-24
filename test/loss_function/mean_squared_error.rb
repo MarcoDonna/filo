aacff6bb3688857b880c35295928b92887a03017
Marco Donna <marco.donna03@gmail.com>
2023-08-24 08:10:58 +0200

    created mean squared error loss function and tests

---
+++ test/loss_function/mean_squared_error.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require 'matrix'
+
+require_relative '../../src/loss_function/mean_squared_error'
+
+class TestMeanSquaredError < Test::Unit::TestCase
+    def test_loss_function
+        predicted = Matrix[[0.2, 0.8], [0.6, -0.2]]
+        observerd = Matrix[[0, 1], [1, 0]]
+        expected = [0.1, 0.04]
+
+        loss_function = MeanSquaredError.new
+        loss = loss_function.loss(predicted, observerd)
+
+        assert_in_delta(0.001, expected[0], loss[0])
+        assert_in_delta(0.001, expected[1], loss[1])
+    end
+
+    def test_loss_derivative
+        predicted = Matrix[[0.2, 0.8], [0.6, -0.2]]
+        observerd = Matrix[[0, 1], [1, 0]]
+        expected = Matrix[[0.4, -0.4], [-0.8, -0.4]]
+        expected = expected.to_a
+
+        loss_function = MeanSquaredError.new
+        loss_derivative = loss_function.loss_derivative(predicted, observerd).to_a
+
+        assert_in_delta(0.001, expected[0][0].abs, loss_derivative[0][0].abs)
+        assert_in_delta(0.001, expected[0][1].abs, loss_derivative[0][1].abs)
+        assert_in_delta(0.001, expected[1][0].abs, loss_derivative[1][0].abs)
+        assert_in_delta(0.001, expected[1][1].abs, loss_derivative[1][1].abs)
+    end
+end
