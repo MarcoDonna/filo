553fa099222e054f9606fae73b958906e4116fa9
Marco Donna <marco.donna03@gmail.com>
2023-08-26 11:52:21 +0200

    created SGD optimizer and tests

---
+++ lib/optimizer/stochastic_gradient_descent.rb
@@ -0,0 +1,22 @@
+require_relative 'optimizer'
+require_relative '../utils/matrix'
+
+class StochasticGradientDescent < Optimizer
+
+    def initialize config
+        super(config)
+        @config[:learning_rate] = 0.01 if @config[:learning_rate].nil?
+    end
+
+    #Use SGD to optimize a Vector af biases using a Vector of gradients.
+    def optimize_biases biases, gradients
+        #delta = -gradient * lr
+        #bias = bias + delta
+        biases + gradients * -@config[:learning_rate]
+    end
+
+    #Use SGD top optimize a Matrix of weights using a Matrix of gradients
+    def optimize_weights weights, gradients
+        weights + gradients * -@config[:learning_rate]
+    end
+end
---
+++ test/optimizer/test_stochastic_gradient_descent.rb
@@ -0,0 +1,30 @@
+require 'test/unit'
+
+require_relative '../../lib/utils/expanded_test_assertions'
+require_relative '../../lib/utils/matrix'
+require_relative '../../lib/optimizer/stochastic_gradient_descent'
+
+class TestStochasticGradientDescent < Test::Unit::TestCase
+
+    def test_optimize_biases
+        biases = Vector[1, -1, 0.5]
+        gradients = Vector[0.8, 0.3, -0.2]
+        expected = Vector[0.6, -1.15, 0.6]
+
+        opt = StochasticGradientDescent.new({learning_rate: 0.5})
+        optimized_biases = opt.optimize_biases(biases, gradients)
+
+        assert_each_in_delta(expected.to_a, optimized_biases.to_a, 0.001)
+    end
+
+    def test_optimize_weights
+        weights = Matrix[[1, -1, 0.5], [1, -1, 0.5]]
+        gradients = Matrix[[0.8, 0.3, -0.2], [0.8, 0.3, -0.2]]
+        expected = Matrix[[0.6, -1.15, 0.6], [0.6, -1.15, 0.6]]
+
+        opt = StochasticGradientDescent.new({learning_rate: 0.5})
+        optimized_weights = opt.optimize_weights(weights, gradients)
+
+        assert_each_in_delta(expected.to_a.flatten,  optimized_weights.to_a.flatten, 0.001)
+    end
+end
