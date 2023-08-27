c0c68d6fcb26084094c06e60aec33be4d420f729
Marco Donna <marco.donna03@gmail.com>
2023-08-27 10:56:52 +0200

    created Matrix initilizer kronecker_delta

--- lib/utils/matrix.rb
+++ lib/utils/matrix.rb
@@ -1,11 +1,17 @@
 require 'matrix'
 
 class Matrix
+
+
+    def self.kronecker_delta diagonal_size
+        Matrix[*Matrix.zero(diagonal_size).map_row { |row, idx| row[idx] = 1; row }]
+    end
+
     def each_row
-        self.to_a.each { |row| yield row }
+        self.to_a.each_with_index { |row, idx| yield row, idx }
     end
 
     def map_row
-        self.to_a.map { |row| yield row }
+        self.to_a.map.each_with_index { |row, idx| yield row, idx }
     end
 end
--- test/utils/test_matrix.rb
+++ test/utils/test_matrix.rb
@@ -3,6 +3,14 @@ require 'test/unit'
 require_relative '../../lib/utils/matrix'
 
 class TestMatrix < Test::Unit::TestCase
+    def test_kronecker_delta
+        expected = Matrix[[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        diag_size = expected.row_size
+
+        matrix = Matrix.kronecker_delta(diag_size)
+        assert_equal(expected, matrix)
+    end
+
     def test_each_row
         sum_of_rows = 0
         matrix = Matrix[[0, 1, 2, 3, 4, 5]]
