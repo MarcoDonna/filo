ef28e8c68ca637495dac48cee1c55253bfcdd6e8
Marco Donna <marco.donna03@gmail.com>
2023-08-28 09:24:47 +0200

    added methods to add Vector to Matrix (row or column wise)

--- lib/utils/matrix.rb
+++ lib/utils/matrix.rb
@@ -1,7 +1,7 @@
 require 'matrix'
+require_relative 'shape_error'
 
 class Matrix
-
 
     def self.kronecker_delta diagonal_size
         Matrix[*Matrix.zero(diagonal_size).map_row { |row, idx| row[idx] = 1; row }]
@@ -13,5 +13,14 @@ class Matrix
 
     def map_row
         self.to_a.map.each_with_index { |row, idx| yield row, idx }
+    end
+
+    def add_vector vec, direction=:row
+        if direction == :row
+            raise ShapeError.new unless self.column_size == vec.size
+            return Matrix[*self.map_row { |row| Vector[*row] + vec }]
+        else
+            self.t.add_vector(vec)
+        end
     end
 end
--- test/utils/test_matrix.rb
+++ test/utils/test_matrix.rb
@@ -1,6 +1,7 @@
 require 'test/unit'
 
 require_relative '../../lib/utils/matrix'
+require_relative '../../lib/utils/shape_error'
 
 class TestMatrix < Test::Unit::TestCase
     def test_kronecker_delta
@@ -30,5 +31,19 @@ class TestMatrix < Test::Unit::TestCase
         output = input.map_row { |row| row[0] + 1 }
 
         assert_equal(expected, output)
+    end
+
+    def test_add_vec_row
+        m = Matrix[[0, 1], [2, 3]]
+
+        assert_raise(ShapeError) { m.add_vector(Vector[2])}
+        assert_equal(Matrix[[1, 0], [3, 2]], m.add_vector(Vector[1, -1]))
+    end
+
+    def test_add_vec_column
+        m = Matrix[[0, 1], [2, 3]]
+
+        assert_raise(ShapeError) { m.add_vector(Vector[2], :column) }
+        assert_equal(Matrix[[1, 1], [2, 2]], m.add_vector(Vector[1, -1], :column))
     end
 end
