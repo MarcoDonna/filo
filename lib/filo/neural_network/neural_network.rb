199cc6239a160d88e57d4c040c4086de8ad199c7
MarcoDonna <marco.donna03@gmail.com>
2023-09-01 11:59:15 +0200

    created methods to optimize all the weights and biases of the network with only one optimizer

--- lib/filo/neural_network/neural_network.rb
+++ lib/filo/neural_network/neural_network.rb
@@ -1,3 +1,17 @@
+#Copyright 2023 Marco Donna
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 module Filo
     class NeuralNetwork
 
@@ -47,8 +61,56 @@ module Filo
 
         # Optimize the weights and biases in the network
         def optimize
+            if(@config[:optimizer].nil?)
+                @layers.each { |layer| layer.optimize if layer.respond_to?(:optimize) }
+            else
+                biases = @config[:optimizer].optimize_biases(flattened_biases(), flattened_biases_gradients())
+                weights = @config[:optimizer].optimize_vector(flattened_weights(), flattened_weights_gradients())
+                distribute_biases(biases)
+                distribute_weights(weights)
+            end
+        end
+
+        private
+
+        def flattened_biases
+            Vector[*@layers.inject([]) { |list, layer| list << (layer.respond_to?(:biases) ? layer.biases.to_a : []) }.flatten]
+        end
+
+        def flattened_biases_gradients
+            Vector[*@layers.inject([]) { |list, layer| list << (layer.respond_to?(:biases_gradient) ? layer.biases_gradient.to_a : []) }.flatten]
+        end
+
+        def distribute_biases biases
+            biases = biases.to_a
             @layers.each do |layer|
-                layer.optimize if layer.respond_to?(:optimize)
+                if(layer.respond_to?(:biases))
+                    layer.biases = Vector[*biases[0, layer.size]]
+                    biases = biases.drop(layer.size)
+                end
+            end
+        end
+
+        def flattened_weights
+            Vector[*@layers.inject([]) { |list, layer| list << (layer.respond_to?(:weights) ? layer.weights.to_a : []) }.flatten]
+        end
+
+        def flattened_weights_gradients
+            Vector[*@layers.inject([]) { |list, layer| list << (layer.respond_to?(:weights_gradient) ? layer.weights_gradient.to_a : []) }.flatten]
+        end
+
+        def distribute_weights weights
+            weights = weights.to_a
+            @layers.each do |layer|
+                if(layer.respond_to?(:weights))
+                    # Reshape weights to matrix [nurons x weights]
+                    current_layer_weights = []
+                    layer.size.times do
+                        current_layer_weights << weights[0, layer.input_size]
+                        weights = weights.drop(layer.input_size)
+                    end
+                    layer.weights = Matrix[*current_layer_weights]
+                end
             end
         end
     end
