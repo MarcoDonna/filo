bbd545a6eb106a81813c118272cac2c93166b1cc
MarcoDonna <marco.donna03@gmail.com>
2023-09-14 09:42:44 +0200

    moved neural network requires to lib/filor.rb

--- lib/filo.rb
+++ lib/filo.rb
@@ -10,12 +10,11 @@ require_relative 'filo/utils/missing_implementation_error'
 require_relative 'filo/utils/invalid_argument_error'
 require_relative 'filo/utils/shape_error'
 
+require_relative 'filo/neural_network/neural_network'
+
 require_relative 'filo/activation'
 require_relative 'filo/layer'
 require_relative 'filo/loss'
 require_relative 'filo/optimizer'
-require_relative 'filo/neural_network'
 
-module Filo
-
-end
+module Filo; end
