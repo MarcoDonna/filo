169e55a68a1f69ebf22fb5a2ddba1f2ab3873691
MarcoDonna <marco.donna03@gmail.com>
2023-09-19 09:54:54 +0200

    improved output layer class

--- lib/filo/layer/output_layer.rb
+++ lib/filo/layer/output_layer.rb
@@ -10,23 +10,23 @@ module Filo
         #
         # Returns a new Intance of OutputLayer.
         #
-        def self.OutputLayer *args
-            if args.size == 1 and args[0].is_a?(Hash)
-                OutputLayer.new(args[0])
-            elsif args.size == 3 and args[2].is_a?(Hash)
-                OutputLayer(args[2].merge(input_size: args[0], size: args[1]))
-            else
-                raise InvalidArgumentError.new
-            end
+        def self.OutputLayer size: nil, input_size: nil, activation_function: nil, loss_function: nil, optimizer: nil
+            return OutputLayer.new(size: size,
+                                   input_size: input_size,
+                                   activation_function: activation_function,
+                                   loss_function: loss_function,
+                                   optimizer: optimizer)
         end
 
         class OutputLayer < DenseLayer
             attr_reader :loss_metric
 
-            def initialize config={} # :notnew:
-                super(config)
-                raise InvalidArgumentError.new("No :loss_function in layer config") if @config[:loss_function].nil?
+            def initialize size: nil, input_size: nil, activation_function: nil, loss_function: nil, optimizer: nil # :notnew:
+                super(size: size, input_size: input_size, activation_function: activation_function, optimizer: optimizer)
 
+                raise ArgumentError.new if loss_function.nil?
+
+                @loss_function = loss_function
                 @loss_metric = []
             end
 
@@ -41,19 +41,19 @@ module Filo
                 raise ShapeError.new unless target.column_size == @output.column_size and target.row_size == @output.row_size
 
                 @target = target
-                @loss_metric << @config[:loss_function].loss(@output, @target)
-                loss_gradient = @config[:loss_function].loss_derivative(@output, @target)
+                @loss_metric << @loss_function.loss(predicted: @output, observed: @target)
+                loss_gradient = @loss_function.loss_derivative(predicted: @output, observed: @target)
                 # Check if the derivative of the activation function is a Vector (sigmoid) or a Matrix (softplus)
                 # Somet things need to change, maybe have method of the activation functions that tells us what the expected output is
-                if(@config[:activation_function].respond_to?(:jacobian?) and @config[:activation_function].jacobian? === true)
+                if(@activation_function.respond_to?(:jacobian?) and @activation_function.jacobian? === true)
                     # Softmax like activation function derivative
-                    applied_activation_derivative = @config[:activation_function].apply_activation_function_derivative(@before_activation)
+                    applied_activation_derivative = @activation_function.apply_activation_function_derivative(@before_activation)
                     # Each item inside applied_activation_derivative is a Matrix, each Matrix is the derivative of one item in the batch.
                     # For each Matrix, perform matrix-vector multiplication to compute the error of the output layer
-                    @error = Matrix[*applied_activation_derivative.map.with_index { |matrix, i| matrix * Vector[*loss_gradient.to_a[i]] }]
+                    @error = applied_activation_derivative.map.with_index { |matrix, i| matrix * Vector[*loss_gradient.to_a[i]] }.to_matrix
                 else
                     # Sigmoid like activation function derivative
-                    @error = @config[:activation_function].apply_activation_function_derivative(@before_activation).hadamard_product(loss_gradient)
+                    @error = @activation_function.apply_activation_function_derivative(@before_activation).hadamard_product(loss_gradient)
                 end
             end
         end
